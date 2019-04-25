e243560eca2a041d377dfc3612f4226ee76bd030
Nikki Tolentino <nikki@envisialearning.com>
2019-04-25 22:26:18 +0800

    added processes for assessments/participants

--- lib/customview2/project.rb
+++ lib/customview2/project.rb
@@ -12,6 +12,18 @@ module Customview2
         post "/api/v1/projects/#{id}/create_campaign", params
       end
 
+      def create_participant(id, params = {})
+        post "/api/v1/projects/#{id}/participants", params
+      end
+
+      def update_participant(id, params = {})
+        post "/api/v1/projects/#{id}/participants/update/#{params[:guid]}", params[:participant]
+      end
+
+      def assessment_status(id, params = {})
+        get "/api/v1/projects/#{id}/participants/status/#{params[:guid]}", params
+      end
+
       def update_contact_lifetime_value(guid, params = {})
         #post "/api/v1/contacts/#{guid}/update_contact_lifetime_value", params
       end
